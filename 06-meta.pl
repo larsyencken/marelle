c472576cadac59087f5694d235c6187c5a9e88d5
Lars Yencken <lars@yencken.org>
2014-07-28 11:00:40 +1000

    Allow meta-packages to vary by platform.

--- 06-meta.pl
+++ 06-meta.pl
@@ -3,13 +3,23 @@
 %  marelle-deps
 %
 
+% meta_pkg(Name, Plat, Deps).
+%   On platform Plat, you can set up Name by meeting Deps.
+:- multifile meta_pkg/3.
+
+
+% meta_pkg(Name, Deps).
+%   On any platform, you can set up Name by meeting Deps.
 :- multifile meta_pkg/2.
 
-pkg(P) :- meta_pkg(P, _).
+meta_pkg(P, _, Deps) :- meta_pkg(P, Deps).
 
-met(P, _) :- meta_pkg(P, Deps), !,
+
+pkg(P) :- meta_pkg(P, _, _).
+
+met(P, Plat) :- meta_pkg(P, Plat, Deps), !,
     maplist(cached_met, Deps).
 
-meet(P, _) :- meta_pkg(P, _), !.
+meet(P, Plat) :- meta_pkg(P, Plat, _), !.
 
-depends(P, _, Deps) :- meta_pkg(P, Deps).
+depends(P, Plat, Deps) :- meta_pkg(P, Plat, Deps).
