58a82d711873c651fb40e8fb6f571047f3955821
myfreeweb <floatboth@me.com>
2014-09-10 09:18:58 +0000

    add freebsd support

--- sudo.pl
+++ sudo.pl
@@ -9,9 +9,21 @@
 sudo_tell(Filename) :-
     expand_path(Filename, ExpFilename),
     join(['cat >', ExpFilename], BashCmd),
+    which('sudo', Sudo),
     process_create(
-        '/usr/bin/sudo',
+        Sudo,
         ['bash', '-c', BashCmd],
         [stdin(pipe(Stream))]
     ),
     tell(Stream).
+
+% sudo_or_empty/1.
+%   Returns an empty string when ran as root, path to sudo with
+%   a trailing space otherwise.
+sudo_or_empty(Command) :-
+    ( bash_output('whoami', root) ->
+        Command = ''
+    ;
+        which('sudo', Sudo),
+        join([Sudo, ' '], Command)
+    ).
